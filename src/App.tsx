20224859a0bd72b2d37db1b53c382e07341622e1
Christopher Langager <clangager@averoinc.com>
2019-04-20 21:53:44 -0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { CreateElectionPage } from './CreateElectionPage';
+import { Flex } from 'rebass';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+export const App: React.FC = () => {
+  return (
+    <Flex justifyContent="center">
+      <div style={{ maxWidth: '800px', width: '100%' }}>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/create">Create</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={HomePage} />
+            <Route path="/create" component={CreateElectionPage} />
+          </div>
+        </Router>
       </div>
-    );
-  }
-}
-
-export default App;
+    </Flex>
+  );
+};
