c997286c87912158f3fe97dd6a918c07c929995f
Christopher Langager <clangager@averoinc.com>
2019-04-22 20:09:06 -0600

    basic drag and drop working

--- src/BallotPage.tsx
+++ src/BallotPage.tsx
@@ -1,22 +1,11 @@
-import React, { useRef, useImperativeHandle, useCallback } from 'react';
+import React, { useCallback } from 'react';
 
 import { useMachine } from '@xstate/react';
 
 import { Machine, assign, DoneInvokeEvent } from 'xstate';
-import {
-  ListElections_listElections_elections,
-  ListElections,
-} from './generated/ListElections';
 import { service } from './service';
 import { ApolloQueryResult } from 'apollo-client';
-import {
-  Card,
-  CardContent,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-} from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { Flex } from 'rebass';
 import { RouterProps, RouteComponentProps } from 'react-router';
@@ -39,7 +28,8 @@ interface CandidateProps {
 
 interface Context {
   electionId: string;
-  candidates: CandidateProps[];
+  candidatesWithoutVotes: CandidateProps[];
+  candidatesWithVotes: CandidateProps[];
 }
 
 interface StateSchema {
@@ -52,7 +42,8 @@ interface StateSchema {
 type Event = {
   type: 'ORDER_CHANGED';
   payload: {
-    candidates: { id: string; name: string; description: string }[];
+    candidatesWithoutVotes: CandidateProps[];
+    candidatesWithVotes: CandidateProps[];
   };
 };
 
@@ -70,7 +61,7 @@ export const ballotMachine = Machine<Context, StateSchema, Event>({
           actions: [
             assign((_ctx, event: DoneInvokeEvent<ApolloQueryResult<GetElections>>) => {
               return {
-                candidates: event.data.data.getElections.elections[0].candidates.map(
+                candidatesWithoutVotes: event.data.data.getElections.elections[0].candidates.map(
                   candidate => ({
                     ...candidate,
                     description: candidate.description ? candidate.description : '',
@@ -93,7 +84,8 @@ export const ballotMachine = Machine<Context, StateSchema, Event>({
           actions: [
             assign((_ctx, event) => {
               return {
-                candidates: event.payload.candidates,
+                candidatesWithoutVotes: event.payload.candidatesWithoutVotes,
+                candidatesWithVotes: event.payload.candidatesWithVotes,
               };
             }),
           ],
@@ -103,34 +95,18 @@ export const ballotMachine = Machine<Context, StateSchema, Event>({
   },
 });
 
-// a little function to help us with reordering the result
-const reorder = (list: any[], startIndex: number, endIndex: number) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
 export interface Props extends RouteComponentProps<{ id: string }> {}
 export const BallotPage: React.FC<Props> = props => {
   const [current, send] = useMachine(
-    ballotMachine.withConfig({}, { electionId: props.match.params.id, candidates: [] })
+    ballotMachine.withConfig(
+      {},
+      {
+        electionId: props.match.params.id,
+        candidatesWithoutVotes: [],
+        candidatesWithVotes: [],
+      }
+    )
   );
-
-  // const onDragEnd = (result: DropResult) => {
-  //   console.log(result);
-  //   // const dragCandidate = current.context.candidates[dragIndex];
-  //   // const newOrder = update(current.context.candidates, {
-  //   //   $splice: [[dragIndex, 1], [hoverIndex, 0, dragCandidate]],
-  //   // });
-
-  //   // send('ORDER_CHANGED', {
-  //   //   payload: {
-  //   //     candidates: newOrder,
-  //   //   },
-  //   // });
-  // };
 
   const onDragEnd = useCallback(
     ({ source, destination }: DropResult) => {
@@ -138,19 +114,77 @@ export const BallotPage: React.FC<Props> = props => {
         return;
       }
 
-      if (source.index === destination.index) {
-        return;
+      const sourceDropableId =
+        source.droppableId === 'candidatesWithoutVotes'
+          ? 'candidatesWithoutVotes'
+          : 'candidatesWithVotes';
+      const destinationDroppableId =
+        destination.droppableId === 'candidatesWithoutVotes'
+          ? 'candidatesWithoutVotes'
+          : 'candidatesWithVotes';
+
+      if (source.droppableId === destination.droppableId) {
+        if (source.index === destination.index) {
+          return;
+        }
+
+        if (source.droppableId === 'candidatesWithoutVotes') {
+          const rearrangedCandidates = reorder(
+            current.context.candidatesWithoutVotes,
+            source.index,
+            destination.index
+          );
+          send({
+            type: 'ORDER_CHANGED',
+            payload: {
+              candidatesWithoutVotes: rearrangedCandidates,
+              candidatesWithVotes: current.context.candidatesWithVotes,
+            },
+          });
+        }
+
+        if (source.droppableId === 'candidatesWithVotes') {
+          const rearrangedCandidates = reorder(
+            current.context.candidatesWithVotes,
+            source.index,
+            destination.index
+          );
+          send({
+            type: 'ORDER_CHANGED',
+            payload: {
+              candidatesWithoutVotes: current.context.candidatesWithoutVotes,
+              candidatesWithVotes: rearrangedCandidates,
+            },
+          });
+        }
+      } else {
+        const [sourceList, destinationList] = move(
+          current.context[sourceDropableId],
+          current.context[destinationDroppableId],
+          source.index,
+          destination.index
+        );
+
+        const payload: {
+          candidatesWithoutVotes: CandidateProps[];
+          candidatesWithVotes: CandidateProps[];
+        } = {
+          candidatesWithoutVotes: [],
+          candidatesWithVotes: [],
+        };
+
+        payload[sourceDropableId] = sourceList;
+        payload[destinationDroppableId] = destinationList;
+
+        console.log(payload);
+
+        send({
+          type: 'ORDER_CHANGED',
+          payload,
+        });
       }
-
-      const rearrangedCandidates = reorder(
-        current.context.candidates,
-        source.index,
-        destination.index
-      );
-
-      send({ type: 'ORDER_CHANGED', payload: { candidates: rearrangedCandidates } });
     },
-    [current.context.candidates, send]
+    [current.context, send]
   );
 
   if (current.matches('loading')) {
@@ -160,11 +194,38 @@ export const BallotPage: React.FC<Props> = props => {
   return (
     <div style={{ padding: '15px' }}>
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId={'storage-area-list'}>
+        <Droppable droppableId={'candidatesWithVotes'}>
           {(provided: DroppableProvided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-              <div>droppable</div>
-              {current.context.candidates.map((candidate, i) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              style={{
+                margin: '25px',
+                padding: '20px',
+                border: '1px red solid',
+                minHeight: '50px',
+              }}
+            >
+              {current.context.candidatesWithVotes.map((candidate, i) => (
+                <Candidate key={candidate.id} index={i} candidate={candidate} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId={'candidatesWithoutVotes'}>
+          {(provided: DroppableProvided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              style={{
+                margin: '25px',
+                padding: '20px',
+                border: '1px green solid',
+                minHeight: '50px',
+              }}
+            >
+              {current.context.candidatesWithoutVotes.map((candidate, i) => (
                 <Candidate key={candidate.id} index={i} candidate={candidate} />
               ))}
               {provided.placeholder}
@@ -195,3 +256,34 @@ const Candidate: React.FC<{ candidate: CandidateProps; index: number }> = ({
     </Draggable>
   );
 };
+
+// a little function to help us with reordering the result
+function reorder(list: any[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
+//Moves an item from one list to another list.
+function move<T>(
+  source: T[],
+  destination: T[],
+  sourceIndex: number,
+  destinationIndex: number
+): [T[], T[]] {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+
+  console.log(sourceClone);
+  console.log(destClone);
+
+  const [removed] = sourceClone.splice(sourceIndex, 1);
+  destClone.splice(destinationIndex, 0, removed);
+
+  console.log(sourceClone);
+  console.log(destClone);
+
+  return [sourceClone, destClone];
+}
